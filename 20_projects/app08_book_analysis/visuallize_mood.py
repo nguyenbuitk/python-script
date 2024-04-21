48aeb5f9b014e15d6301128e001e3731dade7e49
nguyenbuitk <buinguyen23112k@gmail.com>
2024-04-21 12:28:41 +0700

    done app08

--- 20_projects/app08_book_analysis/visuallize_mood.py
+++ 20_projects/app08_book_analysis/visuallize_mood.py
@@ -1,13 +1,39 @@
 import os
+import glob
+import streamlit as st
+import plotly.express as px
+from nltk.sentiment import SentimentIntensityAnalyzer
 
 folder_path = "diary/"
-for filename in os.listdir(folder_path):
+file_names = sorted(os.listdir(folder_path))
+analyzer = SentimentIntensityAnalyzer()
+dates = []
+negativity = []
+positivity = []
+for filename in file_names:
     # Check if item is a file
     if os.path.isfile(os.path.join(folder_path, filename)):
         # Construct the full file path
         file_path = os.path.join(folder_path, filename)
         # Open the file and read its content
         with open(file_path, "r") as file:
-            print(f"File: {filename}")
-            print(file.read())
-            print("=" * 50)
+            content = file.read()
+            scores = analyzer.polarity_scores(content)
+            positivity.append(scores["pos"])
+            negativity.append(scores["neg"])
+            date = file_path.strip(".txt").strip("diary/")
+            dates.append(date)
+print(dates)
+print(negativity)
+print(positivity)
+st.title("Diary Tone")
+st.subheader("Positivity")
+pos_figure = px.line(x=dates, y=positivity,
+                     labels={"x": "Date", "y": "Positivity"})
+st.plotly_chart(pos_figure)
+
+st.subheader("Negativity")
+neg_figure = px.line(x=dates, y=negativity,
+                     labels={"x": "Date", "y": "Negativity"})
+st.plotly_chart(neg_figure)
+
---
+++ example/sorted_file_names.py
@@ -0,0 +1,15 @@
+file_names = [
+    "2023-10-2.txt",
+    "2023-10-23.txt",
+    "2023-10-25.txt",
+    "2023-10-22.txt",
+    "2023-10-24.txt",
+    "2023-10-27.txt",
+    "2023-10-26.txt"
+]
+
+# Sắp xếp danh sách file_names theo ngày tháng năm
+sorted_file_names = sorted(file_names,reverse=True)
+
+# In danh sách file_names sau khi đã sắp xếp
+print(sorted_file_names)

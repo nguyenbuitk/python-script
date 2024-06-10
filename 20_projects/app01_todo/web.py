74cbdd8b323bc0bed0f8bfb38d2dc7f89eb5449f
nguyenbuitk <buinguyen23112k@gmail.com>
2024-06-10 22:57:13 +0700

    update app 01, it's seem done, after a long time

---
+++ 20_projects/app01_todo/web.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import functions
+todos = functions.get_todos()
+
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your productivity")
+
+for index, todo in  enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    print(checkbox)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+new_todo = st.text_input(label="", placeholder="Add new todo ... ")
+if st.button("Add"):
+    todos.append(new_todo + "\n")
+    functions.write_todos(todos)
+    st.experimental_rerun()
+
+print("session_state", st.session_state)

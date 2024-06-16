07a934846e414699b81fe45efdf57bc6ba1677fe
nguyenbuitk <buinguyen23112k@gmail.com>
2024-06-16 13:26:20 +0700

    update app06 and pandas jupyter

---
+++ app06_weather_api/main.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+stations = pd.read_csv("data_small/stations.txt", skiprows=17)
+# select 2 column
+stations = stations[["STAID", "STANAME                                 "]]
+
+@app.route("/")
+def home():
+    return render_template("home.html", data=stations.to_html())
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    # This line filters the DataFrame to include rows where the value in the '    DATE' column is date
+    # squeeze(): remove the title row
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates="    DATE")
+    temperatures = df.loc[df[' Q_TG'] == 0]['    TG'].squeeze() / 10
+    for item in df.iterrows:
+        
+    for temperature in temperatures:
+        return {"station": station,
+                "temperature": temperature}
+        
+    
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    pass
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- example/pandas/pandas_readcsv.py
+++
@@ -1,8 +0,0 @@
-import os
-import pandas as pd
-
-current_directory = os.path.dirname(os.path.abspath(__file__))
-csv_file_path = os.path.join(current_directory, 'data.csv')
-df_csv = pd.read_csv(csv_file_path)
-
-print(df_csv)

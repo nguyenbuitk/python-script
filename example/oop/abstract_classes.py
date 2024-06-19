9090cf06ae337a12dab2bc6a668850bca0493f25
nguyenbuitk <buinguyen23112k@gmail.com>
2024-06-19 21:00:33 +0700

    update OOP example

--- example/oop/abstract_classes.py
+++ example/oop/abstract_classes.py
@@ -1,2 +1,24 @@
-# Cannot be instantiated directly
-# Define
+# An abstract class can be considered a blueprint for other classes.
+# It allows you to create a set of methods that must be created within any child classes built from the abstract class.
+
+from abc import ABC, abstractmethod
+class Polygon(ABC):
+    @abstractmethod
+    def noofsides(self):
+        pass
+
+class Triagle(Polygon):
+    # overriding abstract method
+    def noofsides(self):
+        print("I have 3 sides")
+
+class Pentagon(Polygon):
+    # overriding abstract method
+    def noofsides(self):
+        print("I have 5 sides")
+        
+# Driver code
+R = Triagle()
+R.noofsides()
+K = Pentagon()
+K.noofsides()

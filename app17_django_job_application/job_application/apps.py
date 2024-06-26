c2593f50034d53d24799671cede414f01e9e7c95
nguyenbuitk <buinguyen23112k@gmail.com>
2024-06-26 21:16:05 +0700

    add models for django app

--- app17_django_job_application/job_application/apps.py
+++ app17_django_job_application/job_application/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class JobAppConfig(AppConfig):
+class JobApplicationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'job_app'
+    name = 'job_application'

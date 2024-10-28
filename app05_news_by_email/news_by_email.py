993f3f9a4c53f14e8e01cc1c01a4e221da2d0700
nguyenbuitk <buinguyen23112k@gmail.com>
2024-10-28 22:52:44 +0700

    update file 05

--- app05_news_by_email/news_by_email.py
+++ app05_news_by_email/news_by_email.py
@@ -1,20 +1,23 @@
 import requests
 from send_mail import send_mail
 
-topic = "tesla"
+topic = "vietnamese"
 
-api_key = "72ad054290d64d5f847f4a95df8a631f"
+api_key = "0c6d60564cb04ff4a3869ae2d32eac5d"
 url = f"https://newsapi.org/v2/everything?q={topic}&" \
-    "from=2024-02-16&sortBy=publishedAt&apiKey=" \
-    "72ad054290d64d5f847f4a95df8a631f&language=en"
+    "from=2024-10-20&sortBy=popularity&apiKey=" \
+    "0c6d60564cb04ff4a3869ae2d32eac5d&language=en"
 
 request = requests.get(url)
 content = request.json()
+with open("tmp.txt", "w") as f:
+    f.write(str(content["articles"][:5]))
+    
 
-body = ""
-for article in content["articles"][:20]:
+body = "Subject: Today's news" + "\n"
+for article in content["articles"][:5]:
     if article["title"] is not None: 
-        body = "Subject: Today's news" + "\n" + body + article["title"] + "\n" + article["description"] + "\n" + article["url"] + "\n" + 70*"-" +  2*"\n"
+        body = body + article["title"] + "\n" + article["description"] + "\n" + article["url"] + "\n" + 70*"-" +  2*"\n"
 
 body = body.encode("utf-8")
 send_mail(message=body)

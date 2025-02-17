c482d1e3e72c779373b9784b441e149a804a7cbc
nguyenbuitk <buinguyen23112k@gmail.com>
2025-02-17 22:16:36 +0700

    leetcode #111

---
+++ leetcode/111_minimum_depth_of_binary_tree.py
@@ -0,0 +1,71 @@
+class TreeNode:
+    def __init__(self, val=0, left = None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def minDepth(self, root):
+        
+        def height(node):
+            if not node:
+                return 0
+            left = height(node.left)
+            right = height(node.right)
+            
+            if left and right:
+                return min(left, right) + 1
+            
+            if not left and not right:
+                return 1
+            
+        return height(root)
+
+"""
+Ta sẽ kiểm tra cây ví dụ sau:
+
+        3
+       / \
+      9   20
+         /  \
+        15   7
+🔥 DFS sẽ duyệt theo thứ tự sau:
+
+dfs(3)
+    left = dfs(9) 
+        left = dfs(None) = 0
+        right = dfs(None) = 0
+        → Trả về 1 (vì 9 không có con).
+        
+    right = dfs(20)
+        left = dfs(15)
+            return 1
+        right = dfs(7)
+            return 1
+        return 2 
+    return min(left, right) + 1 = 2
+    
+
+Giả sử sau:
+        1
+       / 
+      2  
+     / 
+    3   
+   /
+  4   
+dfs(1)
+    left = dfs(2) = 3
+        left = dfs(3) = 2
+            left = dfs(4) = 1
+                left = None
+                right = None
+                return 1 
+            right = dfs(None) = 0
+            return max(1,0) + 1 = 2
+           
+        right = None
+        return 3
+    return 4
+"""
+"""

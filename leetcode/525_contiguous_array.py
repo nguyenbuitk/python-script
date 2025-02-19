29b8ce75aeb0e1d166425d775f98f2a88748bf26
nguyenbuitk <buinguyen23112k@gmail.com>
2025-02-19 19:51:43 +0700

    leetcode #525

--- leetcode/525_contiguous_array.py
+++ leetcode/525_contiguous_array.py
@@ -1,11 +1,16 @@
 class Solution:
     def findMaxLength(sef, nums):
-        dict = {}
-        for i in range(len(nums)):
-            dict[i] = [0,0]
+        # d[prefix_sum] = index
+        d = {0: -1}
+        prefix_sum = 0
+        res = 0
         for i in range(len(nums)):
             if nums[i] == 0:
-                
-        print(dict)
+                prefix_sum -= 1
+            else: prefix_sum += 1
+            if prefix_sum in d:
+               res = max(res, i - d[prefix_sum]) 
+            else: d[prefix_sum] = i
+        return res
 solution = Solution()
-solution.findMaxLength([0,1,0,1,1])
+print(solution.findMaxLength([0,0,1,0,0,0,1,1]))
